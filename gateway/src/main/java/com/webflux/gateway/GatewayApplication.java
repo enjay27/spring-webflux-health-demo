c44d2b6be3608c8ed3a6bcff0047f9fceb556eab
Noah <23338687+enjay27@users.noreply.github.com>
2022-10-10 16:09:00 +0900

    change gateway application reactive type

--- gateway/src/main/java/com/webflux/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/webflux/gateway/GatewayApplication.java
@@ -1,13 +1,16 @@
 package com.webflux.gateway;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GatewayApplication.class, args);
+        new SpringApplicationBuilder(GatewayApplication.class)
+                .web(WebApplicationType.REACTIVE)
+                .run(args);
     }
 
 }
